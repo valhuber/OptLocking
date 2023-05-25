45661031c990e8e7c4bd86b1da8a56fb11887be4
Val Huber <valjhuber@gmail.com>
2023-05-24 20:46:41 -0700

    code comments

--- api/system/opt_locking/opt_locking.py
+++ api/system/opt_locking/opt_locking.py
@@ -8,6 +8,7 @@ import logging
 
 import sqlalchemy
 from sqlalchemy import inspect
+from sqlalchemy import event
 from safrs import SAFRSBase
 
 from config import OptLocking
@@ -16,10 +17,11 @@ from config import Config
 logger = logging.getLogger(__name__)
 
 def opt_locking_setup(session):
-    pass
+    """
+    Listen_for read events - set rows' CheckSum property for optimistic locking
 
-    from sqlalchemy import event
-
+    Called at Server start (api_logic_server_run)...
+    """
     @event.listens_for(session, 'loaded_as_persistent')
     def receive_loaded_as_persistent(session, instance):
         "listen for the 'loaded_as_persistent' (get) event - set CheckSum"
@@ -28,7 +30,13 @@ def opt_locking_setup(session):
         setattr(instance, "_check_sum_property", checksum_value)
 
 def checksum(list_arg: list) -> int:
+    """
+    Args:
+        list_arg (list): list of (rows') attribute values
 
+    Returns:
+        int: hash(list values), with special handling for None
+    """
     real_tuple = []
     skip_none = True  # work-around for non-repeatable hash(None)
     if skip_none:     # https://bugs.python.org/issue19224
@@ -43,6 +51,13 @@ def checksum(list_arg: list) -> int:
     return result
 
 def checksum_row(row: object) -> int:
+    """
+    Args:
+        row (object): SQLAlchemy row
+
+    Returns:
+        int: hash(row attributes), using checksum()
+    """
     inspector = inspect(row)
     mapper = inspector.mapper
     iterate_properties = mapper.iterate_properties
@@ -57,6 +72,13 @@ def checksum_row(row: object) -> int:
     return return_value
 
 def checksum_old_row(logic_row_old: object) -> int:
+    """
+    Args:
+        logic_row_old (object): old_row (from LogicBank via declare_logic)
+
+    Returns:
+        int: hash(old_row attributes), using checksum()
+    """
     attr_list = []
     for each_property in logic_row_old.keys():
         logger.debug(f'old_row.property: {each_property} <{type(each_property)}>')
@@ -68,13 +90,28 @@ def checksum_old_row(logic_row_old: object) -> int:
 
 
 def opt_lock_patch(logic_row: LogicRow):
+    """
+    Called by logic/declare_logic in early (before logic) event for updates (patch)
+
+    Compares as_read_checksum to old_row_checksum, to determine whether row changed since read
+
+    - as_read_checksum is submitted in patch by client, from initial get (see receive_loaded_as_persistent)
+    - old_row_checksum is provided by Logicbank - it's the current row on disk
+
+    Args:
+        logic_row (LogicRow): LogicBank row being updated
+
+    Raises:
+        Exception: "Sorry, row altered by another user - please note changes, cancel and retry"
+        Exception: "Optimistic Locking error - required CheckSum not present"
+    """
     logger.debug(f'Opt Lock Patch')
     if hasattr(logic_row.row, "CheckSum"):
         as_read_checksum = logic_row.row.CheckSum
         if as_read_checksum != "!opt_locking_is_patch":
-            current_checksum = checksum_old_row(logic_row.old_row)
-            if as_read_checksum != current_checksum:
-                logger.info(f"optimistic lock failure - as-read vs current: {as_read_checksum} vs {current_checksum}")
+            old_row_checksum = checksum_old_row(logic_row.old_row)
+            if as_read_checksum != old_row_checksum:
+                logger.info(f"optimistic lock failure - as-read vs current: {as_read_checksum} vs {old_row_checksum}")
                 raise Exception("Sorry, row altered by another user - please note changes, cancel and retry")
     else:
         if Config.OPT_LOCKING == OptLocking.OPTIONAL.value:
